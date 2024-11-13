7cc9d71621f287473392810b40d87d1a90a10067
kavyabijja12 <bijjakavya1999@gmail.com>
2024-11-13 16:27:32 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import styles from './App.module.css'
+
+function App() {
+  return (
+    <div className={styles.App}>
+    Hello World!
+    </div>
+  )
+}
+
+export default App
